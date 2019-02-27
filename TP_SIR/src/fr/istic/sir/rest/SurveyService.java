bbb8f8d2a45197e390edd9dc1d1e184cdb3e8fa7
Mathieu Le Han <mathieu.lehan@gmail.com>
2019-02-27 11:03:15 +0100

    REST Sondages OK

--- TP_SIR/src/fr/istic/sir/rest/SurveyService.java
+++ TP_SIR/src/fr/istic/sir/rest/SurveyService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,11 +21,11 @@ import main.java.fr.ensai.tpjpaensai.domain.SondageTypeDateEtLieu;
 import main.java.fr.ensai.tpjpaensai.domain.SondageTypeLieu;
 import main.java.fr.ensai.tpjpaensai.domain.SondageTypeListeChoix;
 
-@Path("/surveys(")
+@Path("/surveys")
 public class SurveyService extends AbstractService<Sondage>{
 
 	private EntityManager em;
-
+	
 	public SurveyService() {
 		super(Sondage.class);
 	} 
@@ -43,21 +46,28 @@ public class SurveyService extends AbstractService<Sondage>{
 	@GET
 	@Produces({ "application/json" })
 	public List<Sondage> findAll() {
-			TypedQuery<SondageTypeDate> date = em.createQuery("SELECT c FROM SondageTypeDate c", SondageTypeDate.class);
-			TypedQuery<SondageTypeDateEtLieu> date_lieu = em.createQuery("SELECT c FROM SondageTypeDateEtLieu c", SondageTypeDateEtLieu.class);
-			TypedQuery<SondageTypeLieu> lieu = em.createQuery("SELECT c FROM SondageTypeLieu c", SondageTypeLieu.class);
-			TypedQuery<SondageTypeListeChoix> liste = em.createQuery("SELECT c FROM SondageTypeListeChoix c", SondageTypeListeChoix.class);
-			
-			List<SondageTypeDate> resultsDate = date.getResultList();
-			List<SondageTypeDateEtLieu> resultsDateLieu = date_lieu.getResultList();
-			List<SondageTypeLieu> resultsLieu = lieu.getResultList();
-			List<SondageTypeListeChoix> resultsListe = liste.getResultList();
-			List<Sondage> resultAll = new ArrayList<>();
-			resultAll.addAll(resultsDate);
-			resultAll.addAll(resultsDateLieu);
-			resultAll.addAll(resultsLieu);
-			resultAll.addAll(resultsListe);
-			return resultAll;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+		em = factory.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx = em.getTransaction();
+		tx.begin();
+		TypedQuery<SondageTypeDate> date = em.createQuery("SELECT c FROM SondageTypeDate c", SondageTypeDate.class);
+		TypedQuery<SondageTypeDateEtLieu> date_lieu = em.createQuery("SELECT c FROM SondageTypeDateEtLieu c", SondageTypeDateEtLieu.class);
+		TypedQuery<SondageTypeLieu> lieu = em.createQuery("SELECT c FROM SondageTypeLieu c", SondageTypeLieu.class);
+		TypedQuery<SondageTypeListeChoix> liste = em.createQuery("SELECT c FROM SondageTypeListeChoix c", SondageTypeListeChoix.class);
+
+		List<SondageTypeDate> resultsDate = date.getResultList();
+		List<SondageTypeDateEtLieu> resultsDateLieu = date_lieu.getResultList();
+		List<SondageTypeLieu> resultsLieu = lieu.getResultList();
+		List<SondageTypeListeChoix> resultsListe = liste.getResultList();
+		List<Sondage> resultAll = new ArrayList<>();
+
+		resultAll.addAll(resultsDate);
+		resultAll.addAll(resultsDateLieu);
+		resultAll.addAll(resultsLieu);
+		resultAll.addAll(resultsListe);
+		tx.commit();
+		return resultAll;
 	}
 
 	@GET
